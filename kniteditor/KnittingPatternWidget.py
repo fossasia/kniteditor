81fbeb5e2e8b4088829a6d72eaa4cc1a540e2f6c
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-07-10 10:22:45 +0200

    flipped like in SVG

--- kniteditor/KnittingPatternWidget.py
+++ kniteditor/KnittingPatternWidget.py
@@ -11,31 +11,31 @@ from .InstructionSVGWidgetCache import default_cache
 class KnittingPatternWidget(FloatLayout):
 
     """The widget to display a knittitng pattern."""
-    
+
     def __init__(self, patterns, **kw):
         super().__init__(**kw)
         self._patterns = patterns
-    
+
     def build(self):
         """Build the UI elements."""
         self.clear_widgets()
         pattern = self._patterns.patterns.at(0)
         layout = GridLayout(pattern)
         cache = default_cache()
-        instruction_height = 20
-        min_x, min_y, max_x, max_y = layout.bounding_box
-        width = max_x - min_x
-        height = max_y - min_y
+        zoom = 20
+        bbox = layout.bounding_box
+        min_y = bbox[1]
+        flip_x = bbox[2]
         instructions = list(layout.walk_instructions())
         instructions.sort(key=lambda i: i.instruction.render_z)
         for instruction in instructions:
             svg = cache.create_svg_widget(instruction.instruction,
                                           size_hint=(None, None))
             self.add_widget(svg)
-            svg.scale = instruction_height / svg.height
+            svg.scale = zoom / svg.height
             # TODO: fixpositioning according to svg rendering
-            right = (width - instruction.x + min_x + 1) * instruction_height
+            right = (flip_x - instruction.x) * zoom
             svg.set_right(right)
-            svg.y = (instruction.y - min_y + 1) * instruction_height
+            svg.y = (instruction.y - min_y) * zoom
 
 __all__ = ["KnittingPatternWidget"]
