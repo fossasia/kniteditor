b2f5439d92ae0a9232d1246a54a8c36f03390dbb
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-07-14 12:29:46 +0200

    removing debug console for tests

--- windows-build/KnitEditor.py
+++ windows-build/KnitEditor.py
@@ -1,11 +1,20 @@
-try:
+import sys
+
+
+def main():
     from kniteditor import main
     main()
-except:
-    import traceback
-    traceback.print_exc()
-    while 1:
-        try:
-            exec(input(">>> "))
-        except:
-            traceback.print_exc()
+
+if len(sys.argv) == 1:
+    try:
+        main()
+    except:
+        import traceback
+        traceback.print_exc()
+        while 1:
+            try:
+                exec(input(">>> "))
+            except:
+                traceback.print_exc()
+else:
+    main()
