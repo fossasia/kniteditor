f5be4156c1a36c6a760adba9ac531fd353288982
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-07-19 00:27:16 +0200

    mac build working

--- mac-build/build.sh
+++ mac-build/build.sh
@@ -5,11 +5,15 @@ HERE="`dirname \"$0\"`"
 
 cd "$HERE"
 
-( cd .. ; kivy setup.py sdist ; kivy -m pip install --user dist/kniteditor-*.tar.gz ; )
+python3 -m pip uninstall -y kniteditor
+( cd .. ; python3 setup.py sdist ; python3 -m pip install --user --upgrade dist/kniteditor-*.tar.gz ; )
 
-python3 -m pip install --user ../test-requirements.txt
+python3 -m pip install --user --upgrade -r ../test-requirements.txt
+
+python3 -m PyInstaller KnitEditor.spec || exit 1
 
 # see http://stackoverflow.com/a/367826/1320237
 # create the .dmg file
+rm -f dist/KnitEditor.dmg
 hdiutil create -srcfolder dist/KnitEditor.app dist/KnitEditor.dmg
 
--- mac-build/install.sh
+++ mac-build/install.sh
@@ -6,10 +6,12 @@ cd "$HERE"
 
 brew install sdl2 sdl2_image sdl2_ttf sdl2_mixer gstreamer
 brew install python3
-pyhon3 -m pip install --user -I Cython==0.23 --install-option="--no-cython-compile"
+python3 -m pip install --upgrade pip
+python3 -m pip install --user -I Cython==0.23 --install-option="--no-cython-compile"
 USE_OSX_FRAMEWORKS=0 pip3 -m pip install --user kivy
 python3 -m pip uninstall -y Cython==0.23
 python3 -m pip install --user -r ../requirements.txt
 python3 -m pip install --user -r ../test-requirements.txt
 python3 -m pip install --user pygame==1.9.2b4
+python3 -m pip install --user PyInstaller
 
