74f37c179c257e286003e9242cfec74430ad9013
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-07-19 23:51:58 +0200

    manually installing the pygame version for el capitan

--- kniteditor/__init__.py
+++ kniteditor/__init__.py
@@ -1,6 +1,6 @@
 """An editor for knitting projects."""
 
-__version__ = "0.0.19"
+__version__ = "0.0.20"
 
 
 def main(*args, **kw):
--- mac-build/install.sh
+++ mac-build/install.sh
@@ -13,10 +13,18 @@ brew install python3
 python3 -m pip install --upgrade pip
 
 echo "# install pygame"
-# see https://bitbucket.org/pygame/pygame/issues/82/homebrew-on-leopard-fails-to-install#comment-636765
-brew install sdl sdl_image sdl_mixer sdl_ttf portmidi
-brew install mercurial || true
-python3 -m pip install $USER hg+http://bitbucket.org/pygame/pygame
+site_packages="`python3 -c \"import sys;print([p for p in sys.path if p.endswith('site-packages')][0])\"`"
+pygame_whl="pygame.zip"
+python3 -m pip uninstall -y pygame || true
+rm -rf "$site_packages"/pygame*
+wget https://github.com/AllYarnsAreBeautiful/kniteditor/releases/download/kivy-mac-binaries-1/pygame-1.9.2b4-cp35-cp35m-macosx_10_9_intel.whl -O "$pygame_whl"
+unzip "$pygame_whl" -d "$site_packages"
+
+### locally compiled pygame version
+### see https://bitbucket.org/pygame/pygame/issues/82/homebrew-on-leopard-fails-to-install#comment-636765
+##brew install sdl sdl_image sdl_mixer sdl_ttf portmidi
+##brew install mercurial || true
+##python3 -m pip install $USER hg+http://bitbucket.org/pygame/pygame
 
 echo "# install kivy dependencies"
 brew install sdl2 sdl2_image sdl2_ttf sdl2_mixer gstreamer
