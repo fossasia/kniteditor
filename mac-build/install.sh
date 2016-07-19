f46973495fb70958a53d44cb8ed43e1adde9d272
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-07-19 12:28:04 +0200

    removed pygame version
    The pygame version somehow never works.
    https://travis-ci.org/AllYarnsAreBeautiful/kniteditor/jobs/145781613#L395

--- mac-build/install.sh
+++ mac-build/install.sh
@@ -21,7 +21,7 @@ USE_OSX_FRAMEWORKS=0 python3 -m pip install $USER kivy
 python3 -m pip uninstall -y Cython==0.23
 python3 -m pip install $USER -r ../requirements.txt
 python3 -m pip install $USER -r ../test-requirements.txt
-python3 -m pip install $USER pygame==1.9.2b4
+python3 -m pip install $USER pygame
 python3 -m pip install $USER PyInstaller
 
 ./build.sh $USER
