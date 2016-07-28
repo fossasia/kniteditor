e6110515755bc38f72e5bd09af2ec3ab840f5173
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-07-28 11:50:27 +0200

    removed --pyargs argument

--- kniteditor/test/conftest.py
+++ kniteditor/test/conftest.py
@@ -9,7 +9,8 @@ import sys
 HERE = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(HERE, "..", ".."))
 
-arguments_unknown_to_kivy = ["--pep8", "--flakes", "--cov=kniteditor"]
+arguments_unknown_to_kivy = ["--pep8", "--flakes", "--cov=kniteditor",
+                             "--pyargs"]
 for argument in arguments_unknown_to_kivy:
     while argument in sys.argv:
         sys.argv.remove(argument)
