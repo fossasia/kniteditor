71fbc66e23695ea72ed9bdf34254b9b14dccd0fd
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-08-03 01:01:42 +0200

    added connaction mockup

--- kniteditor/AYABKnitSettings.py
+++ kniteditor/AYABKnitSettings.py
@@ -9,6 +9,7 @@ class AYABKnitSettings(BoxLayout):
     """Class containing the settings to connect to the AYAB shield."""
     
     machine_type = StringProperty("KH910")
+    communication_selection = StringProperty("USB - COM01")
     colors_layout = ObjectProperty(None)
 
 Factory.register('AYABKnitSettings', cls=AYABKnitSettings)
